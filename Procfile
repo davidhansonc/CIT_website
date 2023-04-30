da55d9b6b4dc4788a9c1a712709b1e23feb5e6ee
David Hanson <davidhanson.c@gmail.com>
2023-04-30 14:23:09 -0700

    updating proc

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn CIT.wsgi
+web: python main.py
--- pcconfig.py
+++ pcconfig.py
@@ -2,7 +2,8 @@ import pynecone as pc
 
 config = pc.Config(
     app_name="CIT",
-    api_url="http://0.0.0.0:54411",
+    api_url="http://0.0.0.0:8000",
     db_url="sqlite:///CIT.db",
     bun_path="$HOME/.bun/bin/bun",
+    # env=pc.Env.DEV,
 )
