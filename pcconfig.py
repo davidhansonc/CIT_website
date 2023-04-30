a39c35036b86e312178ce27bf38cba77ec82ae99
David Hanson <davidhanson.c@gmail.com>
2023-04-30 13:54:07 -0700

    prod build

--- pcconfig.py
+++ pcconfig.py
@@ -2,6 +2,7 @@ import pynecone as pc
 
 config = pc.Config(
     app_name="CIT",
+    api_url="http://192.168.1.1:8000",
     db_url="sqlite:///CIT.db",
-    env=pc.Env.DEV,
+    bun_path="$HOME/.bun/bin/bun",
 )
