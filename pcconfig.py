4bf3599170d63e3aaf23ef2bde3777f326dd3389
David Hanson <davidhanson.c@gmail.com>
2023-04-30 14:00:43 -0700

    api url udpate

--- pcconfig.py
+++ pcconfig.py
@@ -2,7 +2,7 @@ import pynecone as pc
 
 config = pc.Config(
     app_name="CIT",
-    api_url="http://192.168.1.1:8000",
+    api_url="http://0.0.0.0:54411",
     db_url="sqlite:///CIT.db",
     bun_path="$HOME/.bun/bin/bun",
 )
