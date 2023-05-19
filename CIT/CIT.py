dabd7bec6481216c3f9ab041fc142b03f9e07516
David Hanson <davidhanson.c@gmail.com>
2023-05-18 22:11:30 -0700

    page descriptions

--- CIT/CIT.py
+++ CIT/CIT.py
@@ -16,15 +16,37 @@ class State(pc.State):
 # Create the app.
 app = pc.App(state=State, style=style)
 app.add_page(
-    home, route="/",
+    home, 
+    route="/",
     title="The Church in Tucson",
     description="""A local church, the church in Tucson warmly welcomes you. 
     We are believers in the Lord Jesus Christ who have personally received Him as our Savior."""
 )
-app.add_page(faith, route="/faith", title="Our Faith")
-app.add_page(resources, route="/resources", title="Resources")
-app.add_page(contact, route="/contact", title="Contact Us")
+app.add_page(
+    faith, 
+    route="/faith", 
+    title="Our Faith", 
+    description="We hold the faith which is common to all the believers (Titus 1:4, Jude 3)."
+)
+app.add_page(
+    resources, 
+    route="/resources", 
+    title="Resources",
+    description="Ministry links and other helpful resources."
+)
+app.add_page(
+    contact, 
+    route="/contact", 
+    title="Contact Us",
+    description="We warmly welcome you. Come meet with us, or reach out with any questions."
+)
 # app.add_page(media, route="/media")
 # app.add_page(give, route="/calendar", title="Calendar")
-app.add_page(give, route="/give", title="Give")
+app.add_page(
+    give, 
+    route="/give", 
+    title="Give",
+    description="If you feel to give to the church or the work of the ministry, here is some guidance."
+)
+
 app.compile()
